6286ded7f51c5cdf8229089fade84838578fe2c2
Suryansh Kumar <201951158@iiitvadodara.ac.in>
2021-11-09 08:13:15 +0530

    cors

--- public/assets/client.js
+++ public/assets/client.js
@@ -1,5 +1,5 @@
 
-const socket = io('https://lit-bayou-68554.herokuapp.com/');
+const socket = io.connect(window.location.hostname);
 
 let username = prompt("Enter your name to join");
 while(username == null){
